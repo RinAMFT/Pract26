20f35f489038609ca56393271cbd70195e2e15ca
RinAMFT <mucurkaarina@gmail.com>
2025-02-24 20:25:42 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = { 5, 12, 8, 20, 3, 15, 7 };
+            int num = 5;
+            Sravn(mas, num);
+            Console.ReadLine();
+        }
+        static void Sravn(int[] mas, int num)
+        {
+            {
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    if (mas[i] > num)
+                    {
+                        Console.WriteLine($"mas{i} = {mas[i]} > {num}");
+                    }
+                }
+            }
+        }
+    }
+}
