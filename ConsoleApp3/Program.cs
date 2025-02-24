7e03c54b5536e8b374669f9c3729b0e42feb53b4
RinAMFT <mucurkaarina@gmail.com>
2025-02-24 20:25:05 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            int[] mas = new int[size];
+            Foreach(mas);
+            Console.ReadKey();  
+        }
+        static void Foreach(int[] mas)
+        {
+            For(mas);
+            foreach (int element in mas)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        static void For(int[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = i;
+            }
+        }
+    }
+}
