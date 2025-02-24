6ab9abd036d4ddd491fceddd84a6ea908bdb9f39
RinAMFT <mucurkaarina@gmail.com>
2025-02-24 20:24:25 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[10];
+            RandomValue(mas);
+            Output(mas);
+            Console.ReadKey();
+        }
+
+        static void RandomValue(int[] mas)
+        {
+            Random random = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = random.Next(-10, 11);
+            }
+        }
+
+        static void Output(int[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine($"mas[{i}] = {mas[i]}");
+            }
+        }
+    }
+}
